c79de539160a3ffac20d2439eff0366214396303
samueldmp92 <samueldmp92@gmail.com>
2016-10-28 18:17:55 +0200

    Creado implementación de ContactoInterfaz

---
+++ src/fp2/poo/pfpooXXX/ContactoImpl.java
@@ -0,0 +1,49 @@
+package fp2.poo.pfpooXXX;
+
+import fp2.poo.utilidades.CorreoElectronicoInterfaz;
+import fp2.poo.utilidades.DomicilioInterfaz;
+import fp2.poo.utilidades.PersonaInterfaz;
+import fp2.poo.utilidades.TelefonoInterfaz;
+
+/**
+ * Created by Usuario on 28/10/2016.
+ */
+public class ContactoImpl {
+    private TelefonoInterfaz telefono;
+    private PersonaInterfaz persona;
+    private DomicilioInterfaz domicilio;
+    private CorreoElectronicoInterfaz correoElectronico;
+
+    public TelefonoInterfaz getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(TelefonoInterfaz telefono) {
+        this.telefono = telefono;
+    }
+
+    public PersonaInterfaz getPersona() {
+        return this.persona;
+
+    }
+
+    public void setPersona(PersonaInterfaz persona) {
+        this.persona = persona;
+    }
+
+    public DomicilioInterfaz getDomicilio() {
+        return this.domicilio;
+    }
+
+    public void setDomicilio(DomicilioInterfaz domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public CorreoElectronicoInterfaz getCorreoElectronico() {
+        return this.correoElectronico;
+    }
+
+    public void setCorreoElectronico(CorreoElectronicoInterfaz eMail) {
+        this.correoElectronico = eMail;
+    }
+}
