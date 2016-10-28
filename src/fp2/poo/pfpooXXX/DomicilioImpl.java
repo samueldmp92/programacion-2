127f9fa4b75fc15a57bd05a6e2f8588725b1e0d5
samueldmp92 <samueldmp92@gmail.com>
2016-10-28 18:24:28 +0200

    Creado implementación de DomicilioInterfaz

---
+++ src/fp2/poo/pfpooXXX/DomicilioImpl.java
@@ -0,0 +1,16 @@
+package fp2.poo.pfpooXXX;
+
+/**
+ * Created by Usuario on 28/10/2016.
+ */
+public class DomicilioImpl {
+    private String domicilioStr;
+
+    public void setDomicilio(String domicilio) {
+        this.domicilioStr = domicilio;
+    }
+
+    public String getDomicilio() {
+        return this.domicilioStr;
+    }
+}
