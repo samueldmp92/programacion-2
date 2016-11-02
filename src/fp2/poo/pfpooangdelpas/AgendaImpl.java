f8f3176764182f2f5de10015fa822ddb2b2155c9
samuel <samueldmp92@gmail.com>
2016-11-02 16:02:53 +0100

    Modificado AgendaImpl

--- src/fp2/poo/pfpooangdelpas/AgendaImpl.java
+++ src/fp2/poo/pfpooangdelpas/AgendaImpl.java
@@ -7,23 +7,50 @@ import fp2.poo.utilidades.TelefonoInterfaz;
 
 public class AgendaImpl {
     public final static int MAX_NUM_CONTACTOS = 1000;
+    private ArrayList<ContactoInterfaz> listaContactos = null;
+    private int numeroContactosUsados = 0;
+    private telefonoImpl=tmpTelefono;
+
+    public Agenda(String archivo)
+            throws NoSuchElementException, OperacionNoPermitidaExcepcion {
+        this.listaContactos = new ArrayList<ContactoInterfaz>();
+        DatosDeEntrada obj = null;
+        obj = new DatosDeEntrada(archivo);
+        while (obj.hasNext()) {
+            try {
+                ContactoInterfaz contacto = obj.next();
+                this.insertarContacto(contacto);
+                this.numeroContactosUsados = numeroContactosUsados + 1;
+            } catch (OperacionNoPermitidaExcepcion e) {
+// Se ha producido un error en la lectura de un contacto
+// y se captura para seguir leyendo los demás contactos.
+            }
+        }
 
     public void insertarContacto(ContactoInterfaz contacto) throws OperacionNoPermitidaExcepcion {
+        this.listaContactos.add(contacto);
     }
 
     public void modificarTelefono(TelefonoInterfaz telefonoAntiguo,
                                   TelefonoInterfaz telefonoNuevo)
             throws OperacionNoPermitidaExcepcion {
         try {
-            telefonoAntiguo = telefonoNuevo;
-        } catch (Exception OperacionNoPermitidaExcepcion) {
-        }
+        this.tmpTelefono = telefonoNuevo.get;//terminar de definir bien el metodo
+            telefonoAntiguo.set(tmpTelefono);
+        telefonoAntiguo = telefonoNuevo;
+    } catch(Exception OperacionNoPermitidaExcepcion){
     }
+
+}
 
     public void eliminarContacto(TelefonoInterfaz telefono)
             throws OperacionNoPermitidaExcepcion {
         try {
-            telefono = null; //sera asi ?
+            while (i <= numeroContactosUsados) {
+                if (this.listaContactos(i).telefono == telefono) {
+                    listaContactos(i) = null; //sera asi?
+                }
+            }
         } catch (Exception OperacionNoPermitidaExcepcion) {
         }
 
