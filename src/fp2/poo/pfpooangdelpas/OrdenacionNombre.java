3cc99f210909ba8ddc76f237b1d43815922f0f67
samuel <samueldmp92@gmail.com>
2016-11-05 16:57:47 +0100

    Modificado OrdenacionNombre

--- src/fp2/poo/pfpooangdelpas/OrdenacionNombre.java
+++ src/fp2/poo/pfpooangdelpas/OrdenacionNombre.java
@@ -1,21 +1,27 @@
 package fp2.poo.pfpooangdelpas;
 
 import java.util.Comparator;
+import java.util.Collections;
+
 
 public class OrdenacionNombre implements Comparator<char> {
-    private char uno;
-    private char dos;
-    private String unostr;
-    private String dosstr;
+   // private String unostr;
+    //private String dosstr;
+    //private int aux;
+
+    private int comparator(String uno, String dos) {
+        uno.compareToIgnoreCase(dos);
+    }
 
     public OrdenacionNombre(ArrayList agenda, int numeroContacto) {
-        for (i = 0; i <= numeroContacto; i++) {
-            this.unostr = agenda(i).getPersona().getPrimerApellido();
-            this.dosstr = agenda(i + 1).getPersona().getPrimerApellido();
-            this.uno=this.unostr.charAt(0);
-            this.dos=this.dosstr.charAt(0);
+        //for (i = 0; i <= numeroContacto; i++) {
+            //this.unostr = agenda(i).getPersona().getPrimerApellido();
+            //this.dosstr = agenda(i + 1).getPersona().getPrimerApellido();
+            //aux=unostr.comparato(unostr, dosstr);
+            collections.sort(agenda.getPersona().getPrimerApellido(), comparator);
 
-        }
+
+        //}
 
     }
 }
